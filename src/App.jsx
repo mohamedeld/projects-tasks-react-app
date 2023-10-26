0cda20c1e9536d8d65891637c74341113f1b8b28
mohamedeld <mohamed.azoz20010@gmail.com>
2023-10-26 17:07:28 +0300

    add modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,59 @@
-
-import './App.css'
-import NewProject from './components/NewProject'
-import NoProjectSelected from './components/NoProjectSelected'
-import ProjectSidebar from './components/ProjectSidebar'
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectSidebar from "./components/ProjectSidebar";
+import "./App.css";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+  const handleStartAddProject = () => {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  };
+  const handleCancel = () => {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+  const handleAddProject = (projectData)=>{
+    const projectId = uuidv4();
+    const newProject = {
+      ...projectData,
+      id:projectId
+    }
+    setProjectState((prevState)=>{
+      return {
+        ...prevState,
+        selectedProjectId:undefined,
+        projects:[...prevState.projects,newProject]
+      }
+    })
+  }
   
+  let content;
+  if(projectState.selectedProjectId === null){
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancel}/>
+  }else if(projectState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
 
   return (
-    <main className='h-screen my-8 flex gap-8'>
-      <ProjectSidebar/>
-      <NoProjectSelected/>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar onStartAddProject={handleStartAddProject} projects={projectState.projects}/>
+      {content}
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,23 +1,53 @@
+import { useRef } from "react";
 import Button from "./Button";
 import Input from "./Input";
+import Modal from "./Modal";
 
-const NewProject = () => {
+const NewProject = ({onAdd,onCancel}) => {
+    const modalRef = useRef();
+    const titleRef = useRef();
+    const descriptionRef = useRef();
+    const dueDateRef = useRef();
+
+   
+
+    const handleSubmit = ()=>{
+        const enteredTitle = titleRef.current.value;
+        const enteredDescription = descriptionRef.current.value;
+        const enteredDueDate= dueDateRef.current.value;
+        if(enteredTitle.trim() === "" || enteredDescription.trim() ==="" || enteredDueDate.trim() === ""){
+            modalRef.current.open();
+            return;
+        }
+        onAdd({
+            title:enteredTitle,
+            description:enteredDescription,
+            dueDate:enteredDueDate
+        })
+    }
   return (
+    <>
+    <Modal ref={modalRef}>
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">Oops... looks like you forgot to enter a value</p>
+        <p className="text-stone-600 mb-4">Please make sure you provide a valid value for every input field.</p>
+    </Modal>
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
         <li>
-          <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+          <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>Cancel</button>
         </li>
         <li>
-          <Button>+ Add Project</Button>
+          <Button onClick={handleSubmit}>+ Add Project</Button>
         </li>
       </menu>
       <div>
-        <Input label={"Title"} isTextarea={false} type="text" />
-        <Input label={"Description"} isTextarea />
-        <Input label={"Due Date"} isTextarea={false} type="text" />
+        <Input label={"Title"}  type="text" ref={titleRef}/>
+        <Input label={"Description"} isTextarea ref={descriptionRef} />
+        <Input label={"Due Date"}  type="date" ref={dueDateRef} />
       </div>
     </div>
+    </>
   );
 };
 
