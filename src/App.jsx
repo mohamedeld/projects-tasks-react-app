009e8d26452a34311b5c02ad84c7e9bac556d769
mohamedeld <mohamed.azoz20010@gmail.com>
2023-10-26 14:13:40 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import ProjectSidebar from './components/ProjectSidebar'
+
+function App() {
+  
+
+  return (
+    <main className='h-screen my-8'>
+      <ProjectSidebar/>
+    </main>
+  )
+}
+
+export default App
